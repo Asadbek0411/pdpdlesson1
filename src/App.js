bd599b5ead72081bc0ab07c965f61524a54061ef
Asadbek0411 <64528923+Asadbek0411@users.noreply.github.com>
2023-01-10 17:39:35 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div className="container">
+
+          <header>
+
+              <div className="logo"><a href="#">Odam Luiran</a></div>
+
+              <div className="navbar">
+                  <a href="#">Home</a>
+                  <a href="#">About</a>
+                  <a href="#">Contact</a>
+              </div>
+          </header>
+
+          <div className="home">
+
+              <h3>Illustration</h3>
+
+              <div className="img">
+
+                  <div className="img1">
+                      <img src="https://static.wixstatic.com/media/b0a6a8b4cd614ba4b34fef97861ffd94.jpg/v1/fit/w_1206,h_671,q_90/b0a6a8b4cd614ba4b34fef97861ffd94.webp" alt=""/>
+                      <img src="https://static.wixstatic.com/media/c92a3add546747bd88fd5038181d8b69.jpg/v1/fit/w_1044,h_706,q_90/c92a3add546747bd88fd5038181d8b69.webp" alt=""/>
+                      <img src="https://static.wixstatic.com/media/929ccc31ea0c4a17b2528ab130c678bf.jpg/v1/fit/w_1044,h_706,q_90/929ccc31ea0c4a17b2528ab130c678bf.webphttps://static.wixstatic.com/media/929ccc31ea0c4a17b2528ab130c678bf.jpg/v1/fit/w_1044,h_706,q_90/929ccc31ea0c4a17b2528ab130c678bf.webp" alt=""/>
+                  </div>
+                  <div className="img2">
+                      <img src="https://static.wixstatic.com/media/aa0019327c6d4be98654d1f19c65c2d6.jpg/v1/fit/w_1192,h_905,q_90/aa0019327c6d4be98654d1f19c65c2d6.webp" alt=""/>
+                      <img src="https://static.wixstatic.com/media/1e4f7c8f9c9349018a030f86d2e6be7c.jpg/v1/fit/w_1044,h_706,q_90/1e4f7c8f9c9349018a030f86d2e6be7c.webp" alt=""/>
+                      <img src="https://static.wixstatic.com/media/aebeb9ca2b2f41328206f6d49ab83dd3.jpg/v1/fit/w_1044,h_706,q_90/aebeb9ca2b2f41328206f6d49ab83dd3.webp" alt=""/>
+                          </div>
+                  <div className="img3">
+                      <img src="https://static.wixstatic.com/media/560a5087eb674128961368159092afc6.jpg/v1/fit/w_1044,h_706,q_90/560a5087eb674128961368159092afc6.webp" alt=""/>
+                      <img src="https://static.wixstatic.com/media/f9f5a77a1ccc47958da70b18f69f39ea.jpg/v1/fit/w_1044,h_706,q_90/f9f5a77a1ccc47958da70b18f69f39ea.webp" alt=""/>
+                      <img src="https://static.wixstatic.com/media/394a147227b149ce9b74508e009f1255.jpg/v1/fit/w_1044,h_706,q_90/394a147227b149ce9b74508e009f1255.webp" alt=""/>
+                  </div>
+ 
+              </div>
+
+
+          </div>
+
+
+
+          <footer>
+              <h4>© 2023 by Odam Lviran. Proudly created with Wix.com</h4>
+          </footer>
+
+
+    </div>
+
+
+
+    </div>
+  );
+}
+
+export default App;
